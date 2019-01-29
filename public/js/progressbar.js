c73e2283aaf36c38e48481221e5ed2ec4a0cd3a8
Angel <agarcia261@gmail.com>
2019-01-28 20:42:50 -0500

    fixed the format of the bar

--- public/js/progressbar.js
+++ public/js/progressbar.js
@@ -1,7 +1,7 @@
 
 var margin = {top: 30, right: 120, bottom: 0, left: 120},
     width = 960 - margin.left - margin.right,
-    height = 280 - margin.top - margin.bottom;
+    height = 260 - margin.top - margin.bottom;
 
 var x = d3.scale.linear()
     .range([0, width]);
@@ -27,10 +27,12 @@ var svg = d3.select(".calorie-body").append("svg")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+
 svg.append("rect")
     .attr("class", "background")
     .attr("width", width)
     .attr("height", height)
+    .attr("fill", "#eceff1")
     .on("click", up);
 
 svg.append("g")

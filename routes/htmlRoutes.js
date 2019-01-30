1c58c44afeb48b9963b302052aabebc7c314ca9f
Angel <agarcia261@gmail.com>
2019-01-29 21:56:00 -0500

    changes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -13,8 +13,8 @@ module.exports = function(app) {
 
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/ingredient/:id", function(req, res) {
+    db.Ingredients.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
       res.render("example", {
         example: dbExample
       });

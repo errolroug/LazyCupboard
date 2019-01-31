4d413dac2a8fd16f6573ee04ac44f203e20b7b55
ant4u <anthonyimontalto@gmail.com>
2019-01-31 00:27:39 -0500

    fixed 'prettier ' flagged issues

--- test/example.test.js
+++ test/example.test.js
@@ -20,44 +20,61 @@ describe("GET /api/ingredients", function() {
   it("should find all ingredients", function(done) {
     // Add some examples to the db to test with
 
-    
     db.Measurements.bulkCreate([
-      { measurementType: "cup", createdAt: "2019-01-30 19:46:18", updatedAt: "2019-01-30 19:46:18" },
-      { measurementType: "teaspoon", createdAt: "2019-01-30 19:46:18", updatedAt: "2019-01-30 19:46:18" }
+      {
+        measurementType: "cup",
+        createdAt: "2019-01-30 19:46:18",
+        updatedAt: "2019-01-30 19:46:18"
+      },
+      {
+        measurementType: "teaspoon",
+        createdAt: "2019-01-30 19:46:18",
+        updatedAt: "2019-01-30 19:46:18"
+      }
     ]).then(function(measurements) {
-
       db.Ingredients.bulkCreate([
-        { name: "Chicken Breasts", quantity: 2, MeasurementID: null, createdAt: "2019-01-30 19:46:18", updatedAt: "2019-01-30 19:46:18" },
-        { name: "Rice", quantity: 1, MeasurementID: 1, createdAt: "2019-01-30 19:46:18", updatedAt: "2019-01-30 19:46:18" }
+        {
+          name: "Chicken Breasts",
+          quantity: 2,
+          MeasurementID: null,
+          createdAt: "2019-01-30 19:46:18",
+          updatedAt: "2019-01-30 19:46:18"
+        },
+        {
+          name: "Rice",
+          quantity: 1,
+          MeasurementID: 1,
+          createdAt: "2019-01-30 19:46:18",
+          updatedAt: "2019-01-30 19:46:18"
+        }
       ]).then(function() {
         // Request the route that returns all examples
         request.get("/api/ingredients").end(function(err, res) {
           var responseStatus = res.status;
           var responseBody = res.body;
-  
+
           // Run assertions on the response
-  
+
           expect(err).to.be.null;
-  
+
           expect(responseStatus).to.equal(200);
-  
+
           expect(responseBody)
             .to.be.an("array")
             .that.has.lengthOf(2);
-  
+
           expect(responseBody[0])
             .to.be.an("object")
             .that.includes({ name: "Chicken Breasts", quantity: 2 });
-  
+
           expect(responseBody[1])
             .to.be.an("object")
             .that.includes({ name: "Rice", quantity: 1 });
-  
+
           // The `done` function is used to end any asynchronous tests
           done();
-        })
-
+        });
       });
     });
-    });
   });
+});

2359c9309125ca7f15d17b03e4b1c61d08b43b98
Errol Rougier <errolrougier@dyn-160-39-244-64.dyn.columbia.edu>
2019-02-09 12:19:08 -0500

    heroku test

--- server.js
+++ server.js
@@ -8,10 +8,14 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(require("body-parser").urlencoded({ extended: true }));
+app.use(require("body-parser").urlencoded({
+  extended: true
+}));
 app.use(
   require("express-session")({
     secret: "keyboard cat",
@@ -25,7 +29,7 @@ app.use(passport.session());
 
 //this are Global Variables. The one for the user needs to be explicit right after
 //the middleware of session and before any route is called:
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   // res.locals.sucess_msg = req.flash("success_msg");
   // res.locals.error_msg = req.flash("error_msg");
 
@@ -47,17 +51,19 @@ app.set("view engine", "handlebars");
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-var syncOptions = { force: false };
+var syncOptions = {
+  force: false
+};
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
+if (process.env.NODE_ENV === "test" || true) {
   syncOptions.force = true;
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync(syncOptions).then(function () {
+  app.listen(PORT, function () {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
